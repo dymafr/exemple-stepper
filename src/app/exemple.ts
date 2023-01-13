f2cb56d27ef764f37cdfc915f7bc752f510115b5
dymafr <32801166+dymafr@users.noreply.github.com>
2023-01-13 09:58:39 +0100

    1 file modified

--- src/app/exemple.ts
+++ src/app/exemple.ts
@@ -1,25 +1,19 @@
-import { Component, OnInit } from "@angular/core";
-import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
-  selector: "app-exemple-component",
-  templateUrl: "exemple.html",
-  styleUrls: ["exemple.scss"]
+  selector: 'app-exemple-component',
+  templateUrl: 'exemple.html',
+  styleUrls: ['exemple.scss'],
 })
-export class ExempleComponent implements OnInit {
-  premierFormulaire: FormGroup;
-  secondFormulaire: FormGroup;
+export class ExempleComponent {
+  premierFormulaire = this.fb.group({
+    prenom: ['', Validators.required],
+    nom: ['', Validators.required],
+  });
+  secondFormulaire = this.fb.group({
+    email: ['', Validators.email],
+  });
 
-  constructor(private fb: FormBuilder) { }
-
-  ngOnInit() {
-    this.premierFormulaire = this.fb.group({
-      prenom: ['', Validators.required],
-      nom: ['', Validators.required],
-    });
-    this.secondFormulaire = this.fb.group({
-      email: ['', Validators.email]
-    });
-  }
-
+  constructor(private fb: FormBuilder) {}
 }
